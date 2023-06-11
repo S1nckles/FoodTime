7d2c8a2902885c6ed2a8a46e6494555a4919e5e0
Andrey <ilynych0810@gmail.com>
2023-06-11 13:21:51 +0300

    Clearing input after pressing the gps button

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ function dropDownFunc() {
     document.getElementById('myDropdown').classList.toggle('show');
     document.querySelector('#arrowdown').classList.toggle('rotate');
 }
+
+// Clear GPS input
+const gpsBtn = document.querySelector('.gps__btn');
+const gpsInput = document.querySelector('.gps');
+gpsBtn.addEventListener('click', function() {
+    gpsInput.value = '';
+});
